28b375721f21fd17065252f8258f9ce1b56e1a56
Amar Wavare <amarwavare711@gmail.com>
2024-05-19 09:27:02 +0530

    init

---
+++ code/01DSA.c
@@ -0,0 +1,18 @@
+// Print numbers from 1 to 10
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+   clock_t begin = clock();
+   int i;
+
+   for (i = 1; i < 100000; ++i)
+   {
+      printf("%d ", i);
+   }
+   clock_t end = clock();
+   double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+   printf("Time spent: %f seconds\n", time_spent);
+   return 0;
+}
